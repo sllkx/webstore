bff55ed9271869ac044d905ebdb901be710bf63a
a <56597359+sllkx@users.noreply.github.com>
2026-02-18 01:19:44 +0900

    Add files via upload

---
+++ webstore.js
@@ -0,0 +1,6 @@
+document.write(`
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sllkx/webstore@main/css.css">
+<script src="https://cdn.jsdelivr.net/gh/sllkx/webstore@main/html.js"><\/script>
+<script src="https://isai.kr/JS/short.js"><\/script>
+<script src="https://cdn.jsdelivr.net/gh/sllkx/webstore@main/main_asset.js"><\/script>
+`);
